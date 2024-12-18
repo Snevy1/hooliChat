914d92fc220c9fabc0386699f6ee0af2049a965d
Snevy1 <simiyunevily@gmail.com>
2024-12-18 15:34:23 +0300

    imported path module

--- server/routes/MessagesRoutes.js
+++ server/routes/MessagesRoutes.js
@@ -2,6 +2,7 @@ import {Router} from "express"
 import { getMessages, uploadFile } from "../controllers/MessagesController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import multer from "multer"
+import path from "path"
 const messagesRoutes = Router();
 const upload = multer({
     dest: path.join("/tmp", "files")
